fe989e20c2635f48091f7ce8aa75cb6ba60cab91
Martin <gosevski@gmail.com>
2022-10-28 17:21:38 +0100

    Update index.js

--- App/routes/index.js
+++ App/routes/index.js
@@ -2,6 +2,42 @@ const express = require('express');
 const router = express.Router();
 const os = require('os');
 
+class MyClientAssertionCredential {
+  constructor() {
+      let clientAssertion = ""
+      try {
+          clientAssertion = fs.readFileSync(process.env.AZURE_FEDERATED_TOKEN_FILE, "utf8")
+      } catch (err) {
+          console.log("Failed to read client assertion file: " + err)
+          process.exit(1)
+      }
+
+      this.app = new msal.ConfidentialClientApplication({
+          auth: {
+              clientId: process.env.AZURE_CLIENT_ID,
+              authority: `${process.env.AZURE_AUTHORITY_HOST}${process.env.AZURE_TENANT_ID}`,
+              clientAssertion: clientAssertion,
+          }
+      })
+  }
+
+  async getToken(scopes) {
+      const token = await this.app.acquireTokenByClientCredential({ scopes: [scopes] }).catch(error => console.log(error))
+      return new Promise((resolve, reject) => {
+          if (token) {
+              resolve({
+                  token: token.accessToken,
+                  expiresOnTimestamp: token.expiresOn.getTime(),
+              })
+          } else {
+              reject(new Error("Failed to get token silently"))
+          }
+      })
+  }
+}
+
+
+
 const { DefaultAzureCredential } = require("@azure/identity");
 const { AuthorizationManagementClient } = require("@azure/arm-authorization");
 
@@ -10,12 +46,24 @@ const servicePrincipalObjectId = process.env["AZURE_SERVICE_PRINCIPAL_OBJECT_ID"
 
 async function getAppRoleAssignments() {
 
-  const credential = new DefaultAzureCredential();
+  const credential = new MyClientAssertionCredential()
   const client = new AuthorizationManagementClient(credential, subscriptionId);
 
   return client.roleAssignments.listForScope(`subscriptions/${subscriptionId}`, { filter: `assignedTo('{${servicePrincipalObjectId}}')` });
 
 }
+
+async function getAppInfo2(req, res, next) {
+  try {
+    const assignments = await getAppRoleAssignments();
+    res.json(assignments);
+  } 
+  catch (err) {
+    next(err);
+  }
+}
+
+async 
 
 async function getAppInfo(req, res, next) {
 
